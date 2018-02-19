e162d119f2f1303c90274cdaf3d9c33c3827708a
Andreas Fuchs <asf@boinkor.net>
2018-02-19 10:46:12 +0100

    Fill out the rest of the actions, allow expansion/collapsing of nodes
    This makes the mode basically usable and also like 10% better than
    compilation-mode already (:

--- gotest-ui.el
+++ gotest-ui.el
@@ -33,28 +33,23 @@
                            (:constructor gotest-ui--make-test-1))
   (package))
 
-(cl-defun gotest-ui--make-test (&rest args &key status package name &allow-other-keys)
+(cl-defun gotest-ui--make-test (ewoc &rest args &key status package name &allow-other-keys)
   (let ((test (apply #'gotest-ui--make-test-1 :status (or status "run") args)))
-    (ewoc-enter-last gotest-ui--ewoc test)
+    (let ((node (ewoc-enter-last ewoc test)))
+      (setf (gethash test gotest-ui--nodes) node))
     test))
 
 ;;; Data manipulation routines:
 
-(defun gotest-ui-ensure-test (package-name base-name)
+(defun gotest-ui-ensure-test (ewoc package-name base-name)
   (let* ((test-name (format "%s.%s" package-name base-name))
          (test (gethash test-name gotest-ui--tests)))
     (if test
         test
       (setf (gethash test-name gotest-ui--tests)
-            (gotest-ui--make-test :name base-name :package package-name)))))
+            (gotest-ui--make-test ewoc :name base-name :package package-name)))))
 
 ;;;; Mode definition
-
-(defvar gotest-ui-mode-map
-  (let ((m (make-sparse-keymap)))
-    (suppress-keymap m)
-    ;; key bindings go here
-    ))
 
 (define-derived-mode gotest-ui-mode special-mode "go test UI"
   "Major mode for running go test with JSON output."
@@ -68,13 +63,33 @@
   (make-local-variable 'text-property-default-nonsticky)
   (push (cons 'keymap t) text-property-default-nonsticky)
   (hack-dir-local-variables-non-file-buffer)
-
   (use-local-map gotest-ui-mode-map))
+
+(setq gotest-ui-mode-map
+      (let ((m (make-sparse-keymap)))
+        (suppress-keymap m)
+        ;; key bindings go here
+        (define-key m (kbd "TAB") 'gotest-ui-toggle-expanded)
+        m))
+
+;;;; Commands:
+
+(defun gotest-ui-toggle-expanded ()
+  "Toggle expandedness of a test/package node"
+  (interactive)
+  (let* ((node (ewoc-locate gotest-ui--ewoc (point)))
+         (data (ewoc-data node)))
+    (unless (or (null data) (not (gotest-ui-thing-p data)))
+      (message "Not expandable."))
+    (setf (gotest-ui-thing-expanded-p data)
+          (not (gotest-ui-thing-expanded-p data)))
+    (ewoc-invalidate gotest-ui--ewoc node)))
 
 ;;;; Displaying the data:
 
 (defvar-local gotest-ui--tests nil)
 (defvar-local gotest-ui--ewoc nil)
+(defvar-local gotest-ui--nodes nil)
 (defvar-local gotest-ui--process-buffer nil)
 (defvar-local gotest-ui--ui-buffer nil)
 (defvar-local gotest-ui--process nil)
@@ -86,25 +101,26 @@
     (buffer-disable-undo)
     (let ((ewoc (ewoc-create 'gotest-ui--pp-test (format "%s in %s" cmdline default-directory)
                              (substitute-command-keys "\n\\{gotest-ui-mode-map}")))
-          (tests (make-hash-table :test #'equal)))
+          (tests (make-hash-table :test #'equal))
+          (nodes (make-hash-table :test #'eql)))
       (setq gotest-ui--tests tests)
       (setq gotest-ui--ewoc ewoc)
-      )))
+      (setq gotest-ui--nodes nodes))))
 
 (defun gotest-ui (cmdline)
   (interactive "sgo test -json ./...")
   (let* ((name (format "*go test: %s in %s" cmdline default-directory))
-         (buffer (generate-new-buffer name)))
-    (gotest-ui--setup-buffer buffer)
+         (buffer (get-buffer-create name)))
     (switch-to-buffer-other-window buffer)
     (gotest-ui-mode)
+    (gotest-ui--setup-buffer buffer cmdline)
     (setq gotest-ui--process-buffer (generate-new-buffer (format " *gotest-ui: %s in %s" cmdline default-directory)))
     (with-current-buffer gotest-ui--process-buffer
-      (setq gotest-ui--tests tests
-            gotest-ui--ui-buffer buffer))
+      (setq gotest-ui--ui-buffer buffer))
     (setq gotest-ui--process
           (start-process-shell-command name gotest-ui--process-buffer cmdline))
     (set-process-filter gotest-ui--process #'gotest-ui-read-json)
+    (set-process-sentinel gotest-ui--process #'gotest-ui--process-sentinel)
     ;; TODO: set a sentinel!
     ))
 
@@ -116,7 +132,9 @@
   (let ((status (gotest-ui-thing-status test))
         (package (gotest-ui-test-package test))
         (name (gotest-ui-thing-name test)))
-    (insert (format "%s %s.%s" status package name)))
+    (insert (format "%s %s.%s" status package name))
+    (when-let ((elapsed (gotest-ui-thing-elapsed test)))
+      (insert (format " (%fs)" elapsed))))
   (when (gotest-ui-thing-expanded-p test)
     (insert "\n")
     (dolist (line (reverse (gotest-ui-thing-output test)))
@@ -125,24 +143,58 @@
 
 ;;;; Handling input:
 
+(defun gotest-ui--process-sentinel (process event)
+  ;; TODO: update ui buffer?
+  )
+
 (defun gotest-ui-read-json (proc input)
   (let* ((process-buffer (process-buffer proc))
-         (ui-buffer (with-current-buffer process-buffer gotest-ui--ui-buffer)))
-    (with-current-buffer process-buffer
-      ;; insert the chunk of output at the end
-      (save-excursion
-        (goto-char (point-max))
-        (insert input))
-      ;; try to read the next object (which is hopefully complete now):
-      (let ((last-object-start (point)))
-        (condition-case nil
-            (let ((obj (json-read)))
-              (with-current-buffer ui-buffer
-                (gotest-ui-update-test-status obj)))
-          (json-error (goto-char last-object-start)))))))
+         (ui-buffer (with-current-buffer process-buffer gotest-ui--ui-buffer))
+         (inhibit-quit t))
+    (with-local-quit
+      (when (buffer-live-p process-buffer)
+        (with-current-buffer process-buffer
+          (save-excursion
+            ;; insert the chunk of output at the end
+            (goto-char (point-max))
+            (insert input)
+
+            ;; try to read the next object (which is hopefully complete now):
+            (let ((last-object-start (process-mark proc)))
+              (goto-char last-object-start)
+              (cl-loop
+               (condition-case-unless-debug err
+                   (let ((obj (json-read)))
+                     (set-marker (process-mark proc) (point))
+                     (with-current-buffer ui-buffer
+                       (gotest-ui-update-test-status obj)))
+                 (json-error (return))
+                 (wrong-type-argument
+                  ;; sometimes json throws this, ugh
+                  (return))
+                 (error
+                  (message "got other error %s?!" err)
+                  (return)))))))))))
 
 (defun gotest-ui-update-test-status (json)
   (let-alist json
-    (let ((action (intern .Action)))
+    (let ((action (intern .Action))
+          test)
       (case action
-        (run (gotest-ui-ensure-test .Package .Test))))))
+        (run (setq test (gotest-ui-ensure-test gotest-ui--ewoc .Package .Test)))
+        (output (setq test (gotest-ui-ensure-test gotest-ui--ewoc .Package .Test))
+                (push .Output (gotest-ui-thing-output test)))
+        (pass
+         (setq test (gotest-ui-ensure-test gotest-ui--ewoc .Package .Test))
+         (setf (gotest-ui-thing-status test) "pass"
+               (gotest-ui-thing-elapsed test) .Elapsed))
+        (fail
+         (setq test (gotest-ui-ensure-test  gotest-ui--ewoc .Package .Test))
+         (setf (gotest-ui-thing-status test) "fail"
+               (gotest-ui-thing-elapsed test) .Elapsed))
+        (skip
+         (setq test (gotest-ui-ensure-test  gotest-ui--ewoc .Package .Test))
+         (setf (gotest-ui-thing-status test) "skip"
+               (gotest-ui-thing-elapsed test) .Elapsed)))
+      (when test
+        (ewoc-invalidate gotest-ui--ewoc (gethash test gotest-ui--nodes))))))
