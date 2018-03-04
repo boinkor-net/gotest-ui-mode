966bcd696e2a9a4149e76c0c28bd8777058e3dc7
Andreas Fuchs <asf@boinkor.net>
2018-03-04 17:08:55 +0100

    Add a test for the specific problem I saw

--- test/gotest-ui-test.el
+++ test/gotest-ui-test.el
@@ -13,4 +13,15 @@
      "")
     ))
 
+(ert-deftest interrupted-test-output-and-invalidation ()
+  (gotest-ui-test (stdout-marker)
+    (gotest-ui-test-stdout-input
+     stdout-marker
+     "{\"Time\":\"2018-03-04T16:06:29.080756913+01:00\",\"Action\":\"run\",\"Package\":\"foo\",\"Test\":\"TestHi\"}"
+     "{\"Time\":\"2018-03-04T16:06:29.083227527+01:00\",\"Action\":\"output\",\"Package\":\"foo\",\"Test\":\"TestHi\",\"Output\":\"=== RUN\tTestHi\\n\"}"
+     "{\"Time\":\"2018-03-04T16:06:29.083227527+01:00\",\"Action\":\"output\",\"Package\":\"foo\",\"Test\":\"TestHi\",\"Output\":\"Yay!\n\"}"
+     "{\"Time\":\"2018-03-04T16:06:29.083278199+01:00\",\"Action\":\"pass\",\"Package\":\"foo\",\"Test\":\"TestHi\",\"Elapsed\":0.0001}"
+     "{\"Time\":\"2018-03-04T16:06:29.083227527+01:00\",\"Action\":\"run\",\"Package\":\"foo\",\"Test\":\"TestThere\"}"
+     )))
+
 ;;; gotest-ui-test.el ends here
